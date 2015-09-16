31ee69fe468054948f123a3cffec121a4afec752
Daniel Moll <dmoll1974@gmail.com>
2015-09-16 10:58:27 +0200

    added sh file to create demo cluster

---
+++ kubernetes/create-kube-cluster-demo.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+kubectl create -f kubernetes/mongo-service.json
+kubectl create -f kubernetes/mongo-controller.json
+kubectl create -f kubernetes/memcached-service.json
+kubectl create -f kubernetes/memcached-controller.json
+kubectl create -f kubernetes/graphite-ui-service.json
+kubectl create -f kubernetes/graphite-carbon-service.json
+kubectl create -f kubernetes/graphite-controller.json
+kubectl create -f kubernetes/targets-io-service.json
+kubectl create -f kubernetes/targets-io-controller.json
+kubectl create -f kubernetes/jenkins-service.json
+kubectl create -f kubernetes/jenkins-controller.json
+kubectl create -f kubernetes/dropwizard-service.json
+kubectl create -f kubernetes/dropwizard-jmx-service.json
+kubectl create -f kubernetes/dropwizard-controller.json
+gcloud compute firewall-rules create targetsio-3000 --allow=tcp:3000
+gcloud compute firewall-rules create graphite-80 --allow=tcp:80
+gcloud compute firewall-rules create graphite-2003 --allow=tcp:2003
+gcloud compute firewall-rules create dropwizard-8090 --allow=tcp:8090
+
+
+
+
+
+
+
+
