a58dc1bac3681ca2941e6bf177fcc4aa2e94ca80
x090371 <Daniel.Moll@klm.com>
2015-11-02 14:49:40 +0100

    use env variables

---
+++ init-graphite-container-volumes.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -e
+set -x
+
+LOG_DIR=/var/lib/targets-io/graphite/log
+DATA_DIR=/var/lib/targets-io/graphite/storage
+
+VOLUMES_INIT_CONTAINER_NAME=graphite-volumes-init
+
+#rm -rf $LOG_DIR
+#rm -rf $DATA_DIR
+
+# this will fail if one of the volume directories already exists, this prevents you from accidentally deleting your existing data
+mkdir $LOG_DIR
+mkdir $DATA_DIR
+
+#sudo restorecon -Rv /$HOME/graphite-volumes
+#sudo chcon -Rt svirt_sandbox_file_t $LOG_DIR
+#sudo chcon -Rt svirt_sandbox_file_t $DATA_DIR
+
+docker rm -f $VOLUMES_INIT_CONTAINER_NAME || true
+
+docker pull hopsoft/graphite-statsd
+
+docker run -d \
+  --name $VOLUMES_INIT_CONTAINER_NAME \
+  hopsoft/graphite-statsd
+
+sleep 2
+docker logs $VOLUMES_INIT_CONTAINER_NAME
+
+docker cp $VOLUMES_INIT_CONTAINER_NAME:/opt/graphite/storage $DATA_DIR
+mv $DATA_DIR/storage/* $DATA_DIR
+rmdir $DATA_DIR/storage
+
+docker cp $VOLUMES_INIT_CONTAINER_NAME:/var/log $LOG_DIR
+mv $LOG_DIR/log/* $LOG_DIR
+rmdir $LOG_DIR/log
+
+docker rm -f $VOLUMES_INIT_CONTAINER_NAME
+
+echo "Content of storage directory"
+ls -alFtr $DATA_DIR
+
+echo "Content of log directory"
+ls -alFtr $LOG_DIR
+
+echo "Success"
