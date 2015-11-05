749b9eaf6e2d91f9479abcae52d7c843699ebee4
x090371 <Daniel.Moll@klm.com>
2015-11-05 16:30:39 +0100

    added custom product filter

--- public/modules/core/filters/product.client.filter.js
+++ public/modules/core/filters/product.client.filter.js
@@ -2,15 +2,20 @@
 angular.module('core').filter('currentProductToTop', [function () {
   return function (sections, current) {
     var newList = [];
-    angular.forEach(sections, function (section) {
-      if (section.name == current) {
-        newList.unshift(section);
-      }
-      else {
-        newList.push(section);
-      }
-    });
-    return newList;
+    if(current === ''){
+      return sections;
+    }else {
+      var patt = new RegExp('^' + current);
+      angular.forEach(sections, function (section) {
+        if (patt.test(section.name)) {
+          newList.unshift(section);
+        }
+        else {
+          newList.push(section);
+        }
+      });
+      return newList;
+    }
   };
 
   }]);
