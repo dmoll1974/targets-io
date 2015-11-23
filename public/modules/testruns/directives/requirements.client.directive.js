b83ac6d9759e25ffa266d66f1c7be7ea35092115
x090371 <Daniel.Moll@klm.com>
2015-11-23 12:00:27 +0100

    remove ordering

--- public/modules/testruns/directives/requirements.client.directive.js
+++ public/modules/testruns/directives/requirements.client.directive.js
@@ -100,8 +100,8 @@
               });
               var orderedData = params.sorting() ? $filter('orderBy')(data, $scope.tableParams.orderBy()) : data;
               // update table params
-              params.total(orderedData.length);
-              $defer.resolve(orderedData);
+              params.total(data.length);
+              $defer.resolve(data);
             }, 500);
           });
         }
