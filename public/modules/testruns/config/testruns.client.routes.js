2ef6d7b6b1430dce65ba1196a79eac468d7535b3
x090371 <Daniel.Moll@klm.com>
2015-11-27 09:39:25 +0100

    cleaned up and added highlighted rows on hover

--- public/modules/testruns/config/testruns.client.routes.js
+++ public/modules/testruns/config/testruns.client.routes.js
@@ -9,13 +9,13 @@ angular.module('testruns').config([
       templateUrl: 'modules/testruns/views/testruns.client.view.html'
     }).state('requirementsTestRun', {
       url: '/requirements/:productName/:dashboardName/:testRunId/:requirementsResult',
-      templateUrl: 'modules/testruns/views/requirements.client.view.html'
+      template: '<requirements></requirements>'
     }).state('benchmarkPreviousBuildTestRun', {
       url: '/benchmark-previous-build/:productName/:dashboardName/:testRunId/:benchmarkResult',
-      templateUrl: 'modules/testruns/views/benchmark-previous-build.client.view.html'
+      template: '<benchmark-previous-build></benchmark-previous-build>'
     }).state('benchmarkFixedBaselineTestRun', {
       url: '/benchmark-fixed-baseline/:productName/:dashboardName/:testRunId/:benchmarkResult',
-      templateUrl: 'modules/testruns/views/benchmark-fixed-baseline.client.view.html'
+      template: '<benchmark-fixed-baseline></benchmark-fixed-baseline>'
     });
   }
 ]);
