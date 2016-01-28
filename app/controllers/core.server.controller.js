a782eace1e3bacd50792dc5ad251b8d0d78e0107
dmoll1974 <dmoll1974@gmail.com>
2016-01-28 09:55:38 +0100

    removed unnecessary headers

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -4,8 +4,7 @@
  */
 exports.index = function (req, res) {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  res.header('Expires', '-1');
-  res.header('Pragma', 'no-cache');
+
   res.render('index', {
     user: req.user || null,
     request: req
