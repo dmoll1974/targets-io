43aca474783e8c2b3f47f0d90d2e098e7dd9343e
dmoll1974 <dmoll1974@gmail.com>
2016-01-28 09:21:57 +0100

    turn off caching for index page

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -3,6 +3,9 @@
  * Module dependencies.
  */
 exports.index = function (req, res) {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
   res.render('index', {
     user: req.user || null,
     request: req
