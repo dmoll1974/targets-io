1ce08971b274bf761cd08bc82bae42ca988afe83
x090371 <Daniel.Moll@klm.com>
2016-05-09 22:15:13 +0200

    added ng-focus-if

--- config/env/production.js
+++ config/env/production.js
@@ -40,7 +40,8 @@ module.exports = {
                 'public/lib/zeroclipboard/dist/ZeroClipboard.min.js',
                 'public/lib/bootstrap-ui-datetime-picker/dist/datetime-picker.min.js',
                 'public/lib/angular-utils-pagination/dirPagination.js',
-                'public/lib/dygraphs/dygraph-combined-dev.js'
+                'public/lib/dygraphs/dygraph-combined-dev.js',
+                'public/lib/ng-focus-if/focusIf.min.js'
 
 
             ]
