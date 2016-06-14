c4e14aa6df60ad9fe0ce66a42592af2e391aed16
Daniel Moll <dmoll1974@gmail.com>
2016-06-14 11:02:45 +0200

    added jenkins credentials

--- config/env/demo.js
+++ config/env/demo.js
@@ -8,6 +8,8 @@ module.exports = {
 	db: process.env.MONGO_URL,
 	redisHost: process.env.REDIS_SERVICE_HOST,
 	redisPort: process.env.REDIS_SERVICE_PORT,
+	jenkinsUser: process.env.JENKINS_USER,
+	jenkinsPassword: process.env.JENKINS_PASSWORD,
 
 
 	app: {
