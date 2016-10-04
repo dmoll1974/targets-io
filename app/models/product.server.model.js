dc6be4fc9c8254c87bd4eea8a7c1dd4842caa34a
Daniel Moll <dmoll1974@gmail.com>
2016-10-04 13:44:34 +0200

    added remove middleware

--- app/models/product.server.model.js
+++ app/models/product.server.model.js
@@ -3,6 +3,8 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'), Schema = mongoose.Schema,
+    Dashboard = mongoose.model('Dashboard'),
+
     config = require('../../config/config');
 /**
  * Product Schema
@@ -44,5 +46,13 @@ var ProductSchema = new mongoose.Schema({
         read: 'primary',
         safe: {w: 'majority', j: true, wtimeout: 5000} // 2 replicas and 5 seconds timeout from replica
     });
+
+ ProductSchema.pre('remove', function(next) {
+     // 'this' is the client being removed. Provide callbacks here if you want
+     // to be notified of the calls' result.
+     Dashboard.remove({productId: this._id}).exec();
+     next();
+ });
+
 ProductSchema.index({ name: 1 }, { unique: true });
 mongoose.model('Product', ProductSchema);
