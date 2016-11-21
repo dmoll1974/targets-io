845f81adcafef625a1392c91128ff8b83ed2505b
Daniel Moll <dmoll1974@gmail.com>
2016-11-21 16:21:04 +0100

    completed demo config

--- config/env/demo.js
+++ config/env/demo.js
@@ -3,14 +3,23 @@
 module.exports = {
 	isProduction: false,
 	isDemo: true,
-	logLevel: 'error',
+	isDevelopment: false,
+	logLevel: 'warning',
+	graphiteRetentionPeriod: '90d', /*90 days*/
 	graphiteHost: process.env.GRAPHITE_HOST,
 	jenkinsHost: process.env.JENKINS_HOST,
 	db: process.env.MONGO_URL,
+	dbUsername: process.env.MONGO_USER,
+	dbPassword: process.env.MONGO_PASSWORD,
+	graylog : {
+		host: process.env.GRAYLOG_HOST,
+		port: process.env.GRAYLOG_PORT
+	},
 	redisHost: process.env.REDIS_SERVICE_HOST,
 	redisPort: process.env.REDIS_SERVICE_PORT,
 	jenkinsUser: process.env.JENKINS_USER,
 	jenkinsPassword: process.env.JENKINS_PASSWORD,
+	jenkinsSSL: false,
 
 
 	app: {
