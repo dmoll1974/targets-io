b5877b0a9decd6e773a52961b0d58a74f25db984
Daniel Moll <dmoll1974@gmail.com>
2017-01-17 20:15:49 +0100

    removed (unused) get metrics route

--- app/routes/metrics.server.routes.js
+++ app/routes/metrics.server.routes.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   var users = require('../../app/controllers/users.server.controller');
   var metrics = require('../../app/controllers/metrics.server.controller');
   // Metrics Routes
-  app.route('/metrics').get(metrics.list).post(metrics.create);
+  app.route('/metrics').post(metrics.create);
   //users.requiresLogin,
   app.route('/metrics/:metricId').get(metrics.read).put(metrics.update)  //users.requiresLogin, metrics.hasAuthorization,
 .delete(metrics.delete);
