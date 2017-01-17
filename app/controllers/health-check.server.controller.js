8f39bd835c95ee07873b18c7aefe7b4cd1085bb5
Daniel Moll <dmoll1974@gmail.com>
2017-01-17 10:12:44 +0100

    simplified health check call

--- app/controllers/health-check.server.controller.js
+++ app/controllers/health-check.server.controller.js
@@ -2,10 +2,10 @@
 /**
  * Module dependencies.
  */
-var os = require('os');
+//var os = require('os');
 var winston = require('winston');
 
-var host = os.hostname()
+//var host = os.hostname()
 
 exports.healthCheck = healthCheck;
 
@@ -14,5 +14,5 @@ function healthCheck(req, res){
   //winston.info('Node ' + os.hostname() + ' is alive!');
   res .set('Content-type', 'text/plain')
       .status(200)
-      .send('Node '+ host + ' is alive!');
+      .send('Node is alive!');
 }
