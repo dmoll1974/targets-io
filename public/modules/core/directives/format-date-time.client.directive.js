ea2a1479ec92f8812decb6f45879f2d2b88d239d
Daniel Moll <dmoll1974@gmail.com>
2017-01-19 14:47:24 +0100

    changed to format that can be parsed

--- public/modules/core/directives/format-date-time.client.directive.js
+++ public/modules/core/directives/format-date-time.client.directive.js
@@ -4,11 +4,11 @@
     return {
       link: function (scope, element, attrs, ctrl) {
         ctrl.$formatters.unshift(function (modelValue) {
-          return $filter('date')(new Date(modelValue),'EEEE, dd-M-yyyy H:mm:ss');
+          return $filter('date')(new Date(modelValue),'EEE, dd MMM yyyy H:mm:ss');
         });
 
         ctrl.$parsers.unshift(function (viewValue) {
-          return $filter('date')(new Date(viewValue),'EEEE, dd-M-yyyy H:mm:ss');
+          return $filter('date')(new Date(viewValue),'EEE, dd MMM yyyy H:mm:ss');
         });
       },
       restrict: 'A',
