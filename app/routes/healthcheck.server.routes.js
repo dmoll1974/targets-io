2303c27f4f0335d26680ae43cc7f0362d94969f7
Daniel Moll <dmoll1974@gmail.com>
2017-03-14 13:03:58 +0100

    removed health-check route

--- app/routes/healthcheck.server.routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = function (app) {
-  var healthCheck = require('../../app/controllers/health-check.server.controller');
-
-  // Health check route
-  app.route('/health-check/').get(healthCheck.healthCheck);
-
-};
